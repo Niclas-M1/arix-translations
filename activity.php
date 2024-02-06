122bae0c7ad3b0d788d3523caa38c9085c4cd27e
Niclas_M <github@niclas-m.xyz>
2024-02-06 08:31:41 +0100

    übersetzt

--- activity.php
+++ activity.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'account-activity-log' => 'Account Activity Log',
-    'activity-log' => 'Activity Log',
-    'clear-filters' => 'Clear Filters',
-    'metadata' => 'Metadata',
-    'close' => 'Close',
-    'no-logs-server' => 'No activity logs available for this server.'
+    'account-activity-log' => 'Kontoaktivitätsprotokoll',
+    'activity-log' => 'Aktivitätsprotokoll',
+    'clear-filters' => 'Filter löschen',
+    'metadata' => 'Metadaten',
+    'close' => 'Schließen',
+    'no-logs-server' => 'Für diesen Server sind keine Aktivitätsprotokolle verfügbar.'
 ];
